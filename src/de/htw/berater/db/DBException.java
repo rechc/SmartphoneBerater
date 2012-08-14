27353ba35c346fee75b0ba796062933acd864c48
Andreas W <mapsmail@gmx.de>
2012-08-14 16:25:59 +0200

    Exceptionklasse hinzugefügt

---
+++ src/de/htw/berater/db/DBException.java
@@ -0,0 +1,11 @@
+package de.htw.berater.db;
+
+public class DBException extends Exception {
+	
+	private static final long serialVersionUID = -7903138366756139253L;
+
+	public DBException(String msg){
+		super(msg);
+	}
+
+}
