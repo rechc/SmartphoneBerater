517f4792ca264d7e13aa6592f759597b6aeb6ace
Andreas W <mapsmail@gmx.de>
2012-08-25 15:31:56 +0200

    Ausgabetabelle vervollständigt

--- src/de/htw/berater/ui/SmartphoneTableModel.java
+++ src/de/htw/berater/ui/SmartphoneTableModel.java
@@ -12,9 +12,9 @@ public final class SmartphoneTableModel extends AbstractTableModel {
 	private List<Smartphone> phones;
 
 	private String[] columnNames = {
-			"Marke", "Name", "TODO", "TODO", "TODO", "TODO", "TODO", "TODO",
-			"TODO", "TODO", "TODO", "TODO", "TODO", "TODO", "TODO", "TODO",
-			"TODO", "TODO", "TODO", "TODO", "TODO", "TODO"
+			"Marke", "Name", "Preis", "Gewicht", "Farbe", "Material", "Gesprächszeit (2G)", "Display",
+			"Auflösung", "interner Speicher", "RAM", "OS", "MegaPixel", "WLAN", "Bluetooth", "MSExchange",
+			"Spritzwasserfest", "Hardwaretastatur", "Mhz", "Kerne", "GPS", "Kamera"
 	};
 
 	public SmartphoneTableModel() {
@@ -39,6 +39,46 @@ public final class SmartphoneTableModel extends AbstractTableModel {
 			return phone.getBrand();
 		case 1:
 			return phone.getName();
+		case 2:
+			return phone.getPrice();
+		case 3:
+			return phone.getWeight();
+		case 4:
+			return phone.getColor();
+		case 5:
+			return phone.getMaterial();
+		case 6:
+			return phone.getBatteryRuntime();
+		case 7:
+			return phone.getDisplaysize();
+		case 8:
+			return phone.getResolution();
+		case 9:
+			return phone.getInternalMemory();
+		case 10:
+			return phone.getRam();
+		case 11:
+			return phone.getOs();
+		case 12:
+			return phone.getMegapixel();
+		case 13:
+			return phone.isWlan();
+		case 14:
+			return phone.isBluetooth();
+		case 15:
+			return phone.isMsexchange();
+		case 16:
+			return phone.isSplashWaterProof();
+		case 17:
+			return phone.isHardwarekeyboard();
+		case 18:
+			return phone.getMhz();
+		case 19:
+			return phone.getCores();
+		case 20:
+			return phone.isGps();
+		case 21:
+			return phone.isHatKameraEigenschaft();
 		default:
 			return "TODO";
 		}
@@ -50,7 +90,7 @@ public final class SmartphoneTableModel extends AbstractTableModel {
 	}
 
 	@Override
-	public Class<?> getColumnClass(int columnIndex) {
+	public Class<?> getColumnClass(int columnIndex) {//TODO fix warning
 		return Object.class;
 	}
 
